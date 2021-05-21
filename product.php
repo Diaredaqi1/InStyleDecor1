40b46c36cf003f1ede7d235a08347d86050b6056
DetjonaLika <74304169+DetjonaLika@users.noreply.github.com>
2021-05-21 20:53:04 +0200

    Update product.php

--- product.php
+++ product.php
@@ -55,6 +55,7 @@
                 <li> <a href="blog.php">Blog</a></li>
                 <li> <a href="contact.php">Contact us</a></li>
                 <li><a href="cart.php">Cart</a></li>
+                <li><a href="hangman1.php">Game</a></li>
             </ul>
 
         </nav>
