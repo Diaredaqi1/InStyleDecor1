c202342b9402feb0a643fd6d37f7857e9562d7dd
blinasmailaj <74669741+blinasmailaj@users.noreply.github.com>
2021-05-21 20:10:11 +0200

    Create api.php

---
+++ api.php
@@ -0,0 +1,3 @@
+<?php
+echo '<a class="twitter-timeline" data-lang="en" data-width="1200" data-theme="light" href="https://twitter.com/NASA?ref_src=twsrc%5Etfw">Tweets by NASA</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
+?>
