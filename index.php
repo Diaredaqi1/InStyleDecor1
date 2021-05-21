3e65a44ec2fd59c74f439a6ea6c7a1614cbd9237
DetjonaLika <74304169+DetjonaLika@users.noreply.github.com>
2021-05-21 20:27:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -158,6 +158,7 @@ By using our website, you agree to our cookie policy!
                 <li> <a href="blog.php">Blog</a></li>
                 <li> <a href="contact.php">Contact us</a></li>
                 <li><a href="cart.php">Cart</a></li>
+                <li><a href="hangman1.php">Game</a></li>
 
             </ul>
 
